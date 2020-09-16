d1622b58dd085e73657656c3268f3472d1b3a2d1
siddhantparadox <siddhantg314@gmail.com>
2020-09-16 17:27:15 +0530

    Updates

---
+++ precache-manifest.21e737977f43af6d9bd0dc2322df8111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffcc2da8acd379753ed442bdff3f9bc",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "71e2d65ece6283b112a2",
+    "url": "/react-expense-tracker/static/css/main.736d68ac.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/react-expense-tracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e2d65ece6283b112a2",
+    "url": "/react-expense-tracker/static/js/main.95fe6fad.chunk.js"
+  },
+  {
+    "revision": "7edc2b17c6c41405d05a",
+    "url": "/react-expense-tracker/static/js/runtime-main.2e9adeb0.js"
+  }
+]);
